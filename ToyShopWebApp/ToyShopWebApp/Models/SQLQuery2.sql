0778928bd0f6a3fdccb05060513cd15fd30d6323
Jie Ouyang <c3484883@uon.edu.au>
2025-07-18 16:49:27 +0800

    主页，玩具界面

---
+++ ToyShopWebApp/ToyShopWebApp/Models/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿USE ToyShopDB;  -- 确保使用正确的数据库
+
+INSERT INTO Toys (Name, Description, Price, ImageUrl)
+VALUES 
+('Toy Example 1', 'A description of the toy.', 50.00, '/images/toy1.png'),
+('Toy Example 2', 'A description of the toy.', 100.00, '/images/toy2.png'),
+('Toy Example 3', 'A description of the toy.', 88.00, '/images/toy3.png'),
+('Toy Example 4', 'A description of the toy.', 88.00, '/images/toy4.png');
--- ToyShopWebApp/ToyShopWebApp/Models/Toy.cs
+++ ToyShopWebApp/ToyShopWebApp/Models/Toy.cs
@@ -2,10 +2,11 @@
 {
     public class Toy
     {
-        public int Id { get; set; }
+        public int Id { get; set; }       
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
     }
+
 }
